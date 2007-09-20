45dac13b29def7b46b3adfebc6d64daa730e9dd5
nasa42 <nasa42@da871742-c525-0410-b3f9-6bae0572df22>
2007-09-20 12:13:40 +0000

    tagging the configuration.php file for my own configurations.

--- tags/configuration-nasa42.php
+++ tags/configuration-nasa42.php
@@ -1,19 +1,19 @@
 <?php
 DEFINE ('_VALID', 1);
-include "lang_de.php";
-DEFINE ('_LANG', 'de');
-DEFINE('_LOGO', '../images/sms_de.gif');
-// DEFINE('_LOGO', '../images/School.png');
+include "lang_en.php";
+DEFINE ('_LANG', 'en');
+DEFINE('_LOGO', '../images/sms_en.gif');
+
 
 $db_server = 'localhost';
-$db_name = 's2';
-$db_user = 'test';
-$db_password = 'test';
+$db_name = 'swifttide';
+$db_user = 'root';
+$db_password = '';
 
-$SMTP_SERVER = '213.153.32.129';
+$SMTP_SERVER = '';
 $SMTP_USER = '';
 $SMTP_PASSWORD = '';
-$SMTP_FROM_NAME = 'Helmut Leinfellner';
-$SMTP_FROM_EMAIL = 'h.leinfellner@sbg.at';
-$SMTP_REPLY_TO = 'h.leinfellner@sbg.at';
+$SMTP_FROM_NAME = 'admin admin';
+$SMTP_FROM_EMAIL = 'nasa42@gmail.com';
+$SMTP_REPLY_TO = '';
 ?>
