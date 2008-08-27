76174c41201518b516e37e1c1b30cd8623c757e4
nasa42 <nasa42@da871742-c525-0410-b3f9-6bae0572df22>
2008-08-27 16:45:06 +0000

    renaming installation2 to installation. Also made configuration.php just a placeholder for config.php for backward compatibility.

--- configuration.php
+++ configuration.php
@@ -1,19 +1,1 @@
-<?php
-DEFINE ('_VALID', 1);
-include "lang_en.php";
-DEFINE ('_LANG', 'en');
-DEFINE('_LOGO', '../images/sms_en.gif');
-// DEFINE('_LOGO', '../images/School.png');
-
-$db_server = 'localhost';
-$db_name = 'school2';
-$db_user = 'test';
-$db_password = 'test';
-
-$SMTP_SERVER = '213.153.32.129';
-$SMTP_USER = '';
-$SMTP_PASSWORD = '';
-$SMTP_FROM_NAME = 'Helmut Leinfellner';
-$SMTP_FROM_EMAIL = 'h.leinfellner@sbg.at';
-$SMTP_REPLY_TO = 'h.leinfellner@sbg.at';
-?>
+<?php	/**	 * DEPRECATION WARNING: This file is here only for backward compatibility.	 * Please include <tt>config.php</tt> in files instead of this <tt>configuration.php</tt>	 */	require_once('config.php');?>
