1aa346fbd12c60b2f1e845fad4186004eb4f6c87
nasa42 <nasa42@da871742-c525-0410-b3f9-6bae0572df22>
2008-08-27 16:53:28 +0000

    added installation folder. Had to do it in two steps since, I ran in some issues with TortoiseSVN, (Since I just copy/pasted folders and it also copy/pasted .svn folders creating some issues).

---
+++ trunk/config.php
@@ -0,0 +1,12 @@
+<?php
+	
+DEFINE ("_VALID", 1);
+include "lang_en.php";
+DEFINE ("_LANG", "en");
+DEFINE("_LOGO", "../images/sms_en.gif");
+	
+$db_server   = "localhost";
+$db_name     = "swifty";
+$db_user     = "root";
+$db_password = "";
+?>
